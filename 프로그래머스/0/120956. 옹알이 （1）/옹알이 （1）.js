d871510a4c4611807930067a0616727bc57ca85d
zi0w <zzz13588@naver.com>
2024-10-23 12:32:40 +0900

    Update 옹알이 （1）.js

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.js
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -1,3 +1,16 @@
+// 풀이1
+function solution(babbling) {
+    let answer = 0;
+    for (i = 0; i < babbling.length; i++) {
+        if(babbling[i].replace("aya", " ").replace("ye", " ").replace("woo", " ").replace("ma", " ").trim() === "") {
+            answer++
+        }
+    }
+    return answer;
+}
+
+
+// 풀이2
 function solution(babbling) {
     let answer = 0;
     for (i = 0; i < babbling.length; i++) {
