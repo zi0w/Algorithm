087e2a04a9902f4b00825e602b18efed3275c1aa
zi0w <zzz13588@naver.com>
2024-11-08 09:58:09 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,11 @@
+function solution(num) {
+    let count = 0;
+    
+    while (count < 500) {
+        if (num === 1) return count;
+        
+        count++
+        num = num % 2 === 0 ? num / 2 : num * 3 + 1;    
+    }
+    return -1;
+}
