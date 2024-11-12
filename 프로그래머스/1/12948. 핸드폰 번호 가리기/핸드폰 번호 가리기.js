8ecdd8d92d4a76b742ad3bb34214b465bcb1645d
zi0w <zzz13588@naver.com>
2024-11-12 17:24:18 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,11 +1,4 @@
 function solution(phone_number) {
-    let answer = '';
-    for (let i = 0; i < phone_number.length; i++) {
-        if (i < phone_number.length-4) {
-            answer += "*"
-        } else {
-            answer += phone_number[i];    
-        }
-    }
+    let answer = "*".repeat(phone_number.length - 4) + phone_number.slice(-4);
     return answer;
 }
