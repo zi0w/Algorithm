6cb8ceb292a1b54717640833139f1b29c4b7d16e
zi0w <zzz13588@naver.com>
2025-03-13 15:05:18 +0900

    Update 배열 뒤집기.js

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.js
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.js
@@ -1,7 +1,15 @@
+// 1
+
 function solution(num_list) {
     let reverseArr = [];
     for(let i = num_list.length - 1; i >= 0; i--) {
         reverseArr.push(num_list[i]);
     }
     return reverseArr;
+}
+
+// 2
+
+function solution(num_list) {
+    return num_list.reverse();
 }
